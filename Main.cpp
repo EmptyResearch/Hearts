c2376dd1554db7d81c761e678289af42101109c2
Name is private <EmptyResearch@users.noreply.github.com>
2016-08-02 13:43:02 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -14,8 +14,8 @@ int main() {
     cout << "How many players? ";
     array <string, 4> scores = {"0", "0", "0", "0"};
     string input_ = input();
-    if(input=="3"||input=="2"||input=="4") {cout << "Multiplayer currently in devlopment. Sorry."; return 0;} 
-    if(input=="1") int players = 1;
+    if(input_=="3"||input_=="2"||input_=="4") {cout << "Multiplayer currently in devlopment. Sorry."; return 0;} 
+    if(input_=="1") int players = 1;
     array <string, 13> hearts = {"Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts","5 of Hearts","6 of Spad\
     es","7 of Hearts", "8 of Hearts", "9 of Hearts","10 of Hearts","Jack of Hearts","Queen of Hearts","King of Hearts"};
     
@@ -26,7 +26,7 @@ int main() {
     e of Spades", "2 of Spades", "3 of Spades", "4 of Spades","5 of Spades","6 of Spad\
     es","7 of Spades", "8 of Spades", "9 of Spades","10 of Spades","Jack of Spades","Queen of Spades","King of Spades"},hearts};
     
-    array <string, 13> playerOneCards = {
+    array <string, 13> playerOneCards = {cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]],cardIndex[rand()%4[rand()%13]]};
     
     printScores(scores);
     return 0;
